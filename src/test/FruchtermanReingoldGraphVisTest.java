1a19f836b07c5487e309184341b2edbe834f66d8
Shan Huang <sh1338@york.ac.uk>
2014-03-27 10:35:00 +0000

    add test for gml and graphml inputformat

--- src/test/FruchtermanReingoldGraphVisTest.java
+++ src/test/FruchtermanReingoldGraphVisTest.java
@@ -18,6 +18,7 @@
 
 package test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
@@ -30,7 +31,6 @@ import org.apache.giraph.utils.InternalVertexRunner;
 import org.apache.giraph.utils.MockUtils;
 import org.apache.hadoop.io.*;
 import org.junit.Test;
-
 import engine.*;
 
 /**
@@ -55,7 +55,7 @@ public class FruchtermanReingoldGraphVisTest {
 						new VertexValueWritable(), false,
 						computation, 1L);
 		//Mockito.when(SOURCE_ID.get(env.getConfiguration())).thenReturn(2L);
-		vertex.setValue(new VertexValueWritable(new CoordinatesWritable(532,542),new CoordinatesWritable()));
+		vertex.setValue(new VertexValueWritable(new CoordinatesWritable(532,542),new CoordinatesWritable(322,445)));
 		vertex.addEdge(EdgeFactory.create(new IntWritable(2),
 				new EdgeValueWritable()));
 		vertex.addEdge(EdgeFactory.create(new IntWritable(3),
@@ -65,16 +65,16 @@ public class FruchtermanReingoldGraphVisTest {
 		computation.compute(vertex, messages);
 
 		assertTrue(vertex.isHalted());
-		//assertEquals();
+		//assertTrue(!vertex.getValue().getPos().equals(new CoordinatesWritable(Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY)));
 		env.verifyMessageSent(new IntWritable(2), new MessageWritable(new IntWritable(1),vertex.getValue().getPos()));
 		env.verifyMessageSent(new IntWritable(3), new MessageWritable(new IntWritable(1),vertex.getValue().getPos()));
 	}
 
 	/**
-	 * A local integration test on toy data
+	 * A local integration test for CSVEdgeInputFormat
 	 */
-	/*@Test
-	public void testToyData() throws Exception {
+	@Test
+	public void testToyDataCSVEdgeInputFormat() throws Exception {
 
 		// A small graph
 		String[] graph = new String[] { "1,2", "1,3", "1,4", "2,3", "2,4",
@@ -87,8 +87,137 @@ public class FruchtermanReingoldGraphVisTest {
 
 		// Run internally
 		Iterable<String> results = InternalVertexRunner.run(conf, null, graph);
-		System.out.println(results);
+		String output="";
+		for(String result:results){
+			output+=result;
+		}
+		System.out.println(output);
+        //count lines and circles in output
+        int lineCount = 0;
+        int circleCount = 0;
+        String l = "line";
+        String c = "circle";
+        int result = output.indexOf(l);
+        while(result !=-1)
+        {
+        result = output.indexOf(l,result+1);
 
-	}*/
+        lineCount++;
+        }
+        
+        result = output.indexOf(c);
+        while(result !=-1)
+        {
+        result = output.indexOf(c,result+1);
 
+        circleCount++;
+        }
+        //contains 6 edges and 4 vertices
+        assertEquals(6,lineCount);
+        //print circles for each edge and for each vertex, so it's number of edges + number of edges
+        assertEquals(6+4,circleCount);
+	}
+	
+	/**
+	 * A local integration test for GMLEdgeInputFormat
+	 */
+	@Test
+	public void testToyDataGMLEdgeInputFormat() throws Exception {
+
+		// A small graph
+		String[] graph = new String[] { 
+				"edge [ source 2 target 1 ]", 
+				"edge [ source 3 target 1 ]", 
+				"edge [ source 4 target 1 ]"
+				};
+
+		GiraphConfiguration conf = new GiraphConfiguration();
+		conf.setComputationClass(FruchtermanReingoldGraphVis.class);
+		conf.setEdgeInputFormatClass(GMLEdgeInputFormat.class);
+		conf.setVertexOutputFormatClass(SVGVertexTextOutputFormat.class);
+
+		// Run internally
+		Iterable<String> results = InternalVertexRunner.run(conf, null, graph);
+		String output="";
+		for(String result:results){
+			output+=result;
+		}
+		System.out.println(output);
+        //count lines and circles in output
+        int lineCount = 0;
+        int circleCount = 0;
+        String l = "line";
+        String c = "circle";
+        int result = output.indexOf(l);
+        while(result !=-1)
+        {
+        result = output.indexOf(l,result+1);
+
+        lineCount++;
+        }
+        
+        result = output.indexOf(c);
+        while(result !=-1)
+        {
+        result = output.indexOf(c,result+1);
+
+        circleCount++;
+        }
+        //contains 6 edges and 4 vertices
+        assertEquals(3,lineCount);
+        //print circles for each edge and for each vertex, so it's number of edges + number of edges
+        assertEquals(3+4,circleCount);
+	}
+
+	
+	/**
+	 * A local integration test for GraphMLEdgeInputFormat
+	 */
+	@Test
+	public void testToyDataGraphMLEdgeInputFormat() throws Exception {
+
+		// A small graph
+		String[] graph = new String[] { 
+				"<edge id=\""+1+"\" source=\""+2+"\" target=\""+1+"\">", 
+				"<edge id=\""+2+"\" source=\""+3+"\" target=\""+1+"\"/>", 
+				"<edge id=\""+3+"\" source=\""+4+"\" target=\""+1+"\"/>"
+				};
+
+		GiraphConfiguration conf = new GiraphConfiguration();
+		conf.setComputationClass(FruchtermanReingoldGraphVis.class);
+		conf.setEdgeInputFormatClass(GraphMLEdgeInputFormat.class);
+		conf.setVertexOutputFormatClass(SVGVertexTextOutputFormat.class);
+
+		// Run internally
+		Iterable<String> results = InternalVertexRunner.run(conf, null, graph);
+		String output="";
+		for(String result:results){
+			output+=result;
+		}
+		System.out.println(output);
+        //count lines and circles in output
+        int lineCount = 0;
+        int circleCount = 0;
+        String l = "line";
+        String c = "circle";
+        int result = output.indexOf(l);
+        while(result !=-1)
+        {
+        result = output.indexOf(l,result+1);
+
+        lineCount++;
+        }
+        
+        result = output.indexOf(c);
+        while(result !=-1)
+        {
+        result = output.indexOf(c,result+1);
+
+        circleCount++;
+        }
+        //contains 6 edges and 4 vertices
+        assertEquals(3,lineCount);
+        //print circles for each edge and for each vertex, so it's number of edges + number of edges
+        assertEquals(3+4,circleCount);
+	}
 }
